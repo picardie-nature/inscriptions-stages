0adf2d9ef321896b6ed4b15bca9da663b1092fbc
Nicolas Damiens <nicolas@damiens.info>
2012-09-09 16:28:15 +0000

    exporter en csv

--- inscriptions/models.py
+++ inscriptions/models.py
@@ -3,8 +3,45 @@
 from django.db import models
 from django.contrib import admin
 
+import csv
+from django.http import HttpResponse
+
+def export_as_csv_action(description=u"Exporter en CSV les valeurs sélectionnées", fields=None, exclude=None, header=True):
+	"""
+	This function returns an export csv action
+	'fields' and 'exclude' work like in django ModelForm
+	'header' is whether or not to output the column names as the first row
+	"""
+	def export_as_csv(modeladmin, request, queryset):
+		"""
+		Generic csv export admin action.
+		based on http://djangosnippets.org/snippets/1697/
+		"""
+		opts = modeladmin.model._meta
+		field_names = set([field.name for field in opts.fields])
+		if fields:
+			fieldset = set(fields)
+			field_names = field_names & fieldset
+		elif exclude:
+			excludeset = set(exclude)
+			field_names = field_names - excludeset
+
+		response = HttpResponse(mimetype='text/csv')
+		response['Content-Disposition'] = 'attachment; filename=%s.csv' % unicode(opts).replace('.', '_')
+
+		writer = csv.writer(response)
+		if header:
+			writer.writerow(list(field_names))
+		for obj in queryset:
+			writer.writerow([unicode(getattr(obj, field)).encode("utf-8","replace") for field in field_names])
+		return response
+
+	export_as_csv.short_description = description
+	return export_as_csv
+
 class InscritAdmin(admin.ModelAdmin):
 	list_display = ['nom','prenom','stage','adherent']
+	actions = [export_as_csv_action()]
 
 class Inscrit(models.Model):
 	nom = models.CharField(max_length=40, verbose_name="Nom")
