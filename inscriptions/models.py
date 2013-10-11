a227aa25d0bd40cdbc5b089e2a934bbaa32a542f
Nicolas Damiens <nicolas@damiens.info>
2013-10-11 13:03:57 +0000

    formulaire initiation 2014

--- inscriptions/models.py
+++ inscriptions/models.py
@@ -50,7 +50,7 @@ class Inscrit(models.Model):
 	email = models.EmailField(verbose_name="Adresse email")
 	telephone = models.CharField(max_length=10, verbose_name="Numéro de téléphone")
 	stage = models.IntegerField(choices = ((2,'Aisne'),(80,'Somme')), verbose_name="Département où vous souhaitez participer")
-	pratique_naturaliste = models.TextField(verbose_name="Pratiquez-vous une activitée naturaliste ?", blank=True)
+	pratique_naturaliste = models.TextField(verbose_name="Pratiquez-vous une activité naturaliste ?", blank=True)
 	pref_oiseaux = models.BooleanField(verbose_name="Intéressé par les oiseaux")
 	pref_mam_terr = models.BooleanField(verbose_name="Intéressé par les mammifères terrestres")
 	pref_mam_marin = models.BooleanField(verbose_name="Intéressé par les mammifères marins")
@@ -58,7 +58,7 @@ class Inscrit(models.Model):
 	pref_insectes = models.BooleanField(verbose_name="Interessé par les insectes")
 	motivations = models.TextField(verbose_name="Qu'est-ce qui motive votre inscription ?")
 	moyen_comm = models.TextField(blank=True, verbose_name="Comment avez-vous découvert les stages ?")
-	covoiturage = models.BooleanField(verbose_name="Souhaitez-vous participer a un système de covoiturage (échanges de numéros téléphone entre participants au stage) ?")
+	covoiturage = models.BooleanField(True, verbose_name="Vous participez a un système de covoiturage (échanges de numéros téléphone entre participants au stage)")
 	commentaire = models.TextField(blank=True, verbose_name="Avez-vous une question ou une remarque ?")
 	adherent = models.BooleanField(verbose_name="Êtes-vous adhérent de Picardie-Nature ?")
 
